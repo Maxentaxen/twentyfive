f838fa508d436d509632b421d180e4ff0cf2833a
Maxentaxen <max.niemi@elev.ga.ntig.se>
2025-03-13 11:54:08 +0100

    Lite småändringar 😄

--- js/script.js
+++ js/script.js
@@ -4,3 +4,10 @@ button.addEventListener('click', function() {
     this.classList.toggle('open');
     navbar.classList.toggle('visible');}
 );
+
+document.onkeydown = function(event) {
+    if (event.key === 'Escape' && navbar.classList.contains('visible')) {
+        navbar.classList.toggle('visible');
+        button.classList.toggle('open');
+    } 
+}
