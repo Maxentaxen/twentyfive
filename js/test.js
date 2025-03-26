356caca0a6517fceff21707840a2542602325c82
Maxentaxen <167980749+Maxentaxen@users.noreply.github.com>
2025-03-26 21:57:28 +0100

    tog bort testsaker

--- js/test.js
+++
@@ -1,9 +0,0 @@
-let box = document.querySelector('.box');
-let button = document.querySelector('.button')
-button.addEventListener('click', myFunc);
-
-function myFunc(){
-    box.style.borderRadius = "0px";
-    box.style.left = "0px";
-    box.style.height = "100%";
-}
